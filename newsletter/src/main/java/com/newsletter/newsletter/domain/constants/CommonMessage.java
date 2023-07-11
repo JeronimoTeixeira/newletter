5076bd54d74c9fe94ba4845ce4a83bd85847d972
Jeronimo Teixeira <geteixeira095@gmail.com>
2023-07-10 22:12:03 -0300

    refactor: creating error messages

---
+++ newsletter/src/main/java/com/newsletter/newsletter/domain/constants/CommonMessage.java
@@ -0,0 +1,6 @@
+package com.newsletter.newsletter.domain.constants;
+
+public class CommonMessage {
+    public static final String ERRO_DYNAMO_DB_NOT_FOUND = "An error occurred while finding a database. Try again later.";
+    public static final String ERRO_DYNAMO_DB_GENERIC = "An unexpected error occurred. Try again later.";
+}
--- newsletter/src/main/java/com/newsletter/newsletter/services/ClientEmailService.java
+++ newsletter/src/main/java/com/newsletter/newsletter/services/ClientEmailService.java
@@ -5,8 +5,12 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.model.AmazonDynamoDBException;
+import com.newsletter.newsletter.domain.constants.CommonMessage;
 import com.newsletter.newsletter.domain.dynamo.Client;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -24,7 +28,12 @@ public class ClientEmailService {
             dynamoDBMapper.save(contactClient);
         }
         catch (AmazonDynamoDBException exception) {
-            throw exception;
+            switch (exception.getStatusCode()){
+                case 400:
+                    throw new ResponseStatusException(HttpStatusCode.valueOf(exception.getStatusCode()), CommonMessage.ERRO_DYNAMO_DB_GENERIC);
+                default:
+                    throw new ResponseStatusException(HttpStatusCode.valueOf(exception.getStatusCode()), CommonMessage.ERRO_DYNAMO_DB_NOT_FOUND);
+            }
         }
     }
 
